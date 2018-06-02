616e7963d29925246b8cba9c95314e598a73e3fe
Wesley Yung <ysywesley@gmail.com>
2018-06-03 00:24:20 +0800

    update

--- Javascript/repeatbg.js
+++ Javascript/repeatbg.js
@@ -1,16 +1,16 @@
 //repeat button
 
+
+
 function draw(direction) {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     ctx.clearRect(0, 0, c.width, c.height);
-    //var pic = $("").val();
-    var pic = $("select").change(function(){
-        $(this[this.selectedIndex]).val();
-       });
-    var img = document.getElementById("pic");
+    var e = document.getElementById("hero");
+    var value = e.options[e.selectedIndex].value;
+    var img = document.getElementById(value);
     var pat = ctx.createPattern(img, direction);
-    ctx.globalAlpha = 0.4; 
+    //ctx.globalAlpha = 0.8;
     ctx.rect(0, 0, c.width, c.height);
     ctx.fillStyle = pat;
     ctx.fill();
